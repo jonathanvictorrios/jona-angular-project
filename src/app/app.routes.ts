6ba3fa6b0a9d299edadfecc49dbb1209f7fd3f9f
jonavictorrios <jonathan.rios-ls@alitaware.com>
2025-08-21 22:29:50 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Menu } from './menu/menu';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'menu', component: Menu }
+];
+
+
